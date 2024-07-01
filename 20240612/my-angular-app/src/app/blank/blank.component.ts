6565c10ed8993a84df215bb51ed20d1b6d26f460
hebemilanesio1 <134965584+hebemilanesio1@users.noreply.github.com>
2024-07-01 18:18:34 -0300

    Delete 20240612/my-angular-app directory

--- 20240612/my-angular-app/src/app/blank/blank.component.ts
+++
@@ -1,8 +0,0 @@
-// blank.component.ts
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-blank',
-  template: '<p>Página en blanco</p>',
-})
-export class BlankComponent {}
